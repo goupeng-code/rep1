1165dfda8224aab7ce456bfbe5e5ce759e236a03
398220720@qq.com <398220720@qq.com>
2020-05-09 00:14:54 +0800

    c

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[]args){
-		System.out.println("Hello Mysql, Hello Java, Hello Hadoop");
+		System.out.println("Hello Mysql, Hello Mysql, Hello Hadoop");
 	}
 }
